30da289c14dc812f81b5be3f32b1214684e3bbbd
Chech3 <joseangelgarces123@gmail.com>
2024-10-28 00:49:37 -0400

    Es un orm cualquiera XD, pero igual terminare de practicarlo

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import {PirsmaClient} from "@prisma/client"
+import {PrismaClient} from "@prisma/client"
 
-const prisma = new PirsmaClient()
+const prisma = new PrismaClient()
+
+async function main() {
+    const newUser = await prisma.user.create({
+        data: {
+            name: "Juan",
+            email: "ao@example.com"
+        }
+    })
+    console.log(newUser);
+}
+
+async function getData() {
+    const users = await prisma.user.findMany()
+    console.log(users);
+
+    users.map((user) => {
+        console.log(`${user.id} - ${user.name}`);
+    })
+}
+
+getData();
