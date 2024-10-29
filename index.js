c1193c73ecb43bf92979a9799bd2c6e2dfcb5cfa
Chech3 <joseangelgarces123@gmail.com>
2024-10-28 22:11:50 -0400

    uperstDta

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
@@ -14,11 +14,81 @@ async function main() {
 
 async function getData() {
     const users = await prisma.user.findMany()
-    console.log(users);
+    
 
     users.map((user) => {
-        console.log(`${user.id} - ${user.name}`);
+        console.log(`${user.id} - ${user.name} - ${user.last_name} ${user.email}`);
     })
 }
+
+//De eesta forma podemos buscar un único elemento por id
+async function getDataById(id) {
+    const user = await prisma.user.findFirst({
+        where: {
+            id: id
+        }
+    })
+    console.log(user);
+}
+
+//
+async function getDataOr() {
+    const user = await prisma.user.findFirst({
+        where: {
+            AND: [
+                { name: "Juan" },
+                { email: "o@example.com" }
+            ]
+        }
+    })
+    console.log(user);
+}
+
+async function deleteData() {
+    try {
+        const user = await prisma.user.delete({
+            where: {
+                id: 1
+            }
+        })
+        console.log(user);
+    } catch (error) {
+        console.log(error.meta.cause);
+    }
+  
+}
+
+async function updateData() {
+    const user = await prisma.user.update({
+        where: {
+            id: 2
+        },
+        data: {
+            last_name: "apellido"
+        }
+    })
+    console.log(user);
+}
+
+
+async function upsertData() {
+    const user = await prisma.user.upsert({
+        where: {
+            email: "email@example.com",
+        },
+        create:{
+            email: "email@example.com",
+            name: "Nuevo nombre"
+        },
+        update:{
+            name: "Nuevo nombre",
+            last_name: "Nuevo apellido"
+        }
+    })
+    console.log(user);
+}
+
+
+
 
 getData();
